c581c007fb1b675340d85510197f63f532cfc1ae
Yoursadik-nazrul <sadikull3@gmail.com>
2024-04-29 11:55:17 +0600

    Load all posts form the given API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log('Connected');
+// Load all posts form the given API
+const loadPost = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await response.json();
+    const posts = data.posts;
+    console.log(posts);
+}
+
+loadPost();
